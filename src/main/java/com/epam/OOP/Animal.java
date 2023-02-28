8ce3be93fe368fd240ff1005cd05d83517c08685
iskendermazitov <isken.majitov1@gmail.com>
2023-02-28 21:24:46 +0600

    OOP tasks - done

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -2,4 +2,27 @@ package com.epam.OOP;
 
 public class Animal {
 
+  private String color;
+  private int numberOfPaws;
+  private boolean hasFur;
+
+  public Animal(String color, int numberOfPaws, boolean hasFur) {
+    this.color = color;
+    this.numberOfPaws = numberOfPaws;
+    this.hasFur = hasFur;
+  }
+
+  public String getDescription() {
+    var hasFurToString = this.hasFur ? "a" : "no";
+    var pluralSuffix = this.numberOfPaws == 1 ? "" : "s";
+    var description = String.format(
+        "This animal is mostly %s. It has %d paw%s and %s fur.",
+        this.color, this.numberOfPaws, pluralSuffix,hasFurToString);
+    return description;
+  }
+
+  public static void main(String[] args) {
+    var animal = new Animal("black", 4, true);
+    System.out.println(animal.getDescription());
+  }
 }
--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,5 +1,17 @@
 package com.epam.OOP;
 
-public class Dog {
+public class Dog extends Animal{
+
+  public Dog(){
+    super("brown", 4, true);
+  }
+
+//  public Dog(String color, int numberOfPaws, boolean hasFur) {
+//    super(color, numberOfPaws, hasFur);
+//  }
+
+  public static void main(String[] args) {
+    System.out.println(new Dog().getDescription());
+  }
 
 }
